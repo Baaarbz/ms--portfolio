f1b6ff9f93bcc01967dd8be4320edf3fe8f14704
barbz <eduardob907@gmail.com>
2023-11-01 13:23:27 +0100

    improve and refactor table definitions

--- src/main/resources/db/migration/V002__create_company_table.sql
+++ src/main/resources/db/migration/V002__create_company_table.sql
@@ -7,8 +7,13 @@ create table companies
     url         varchar                  not null,
     current_job boolean                  not null,
     start_date  timestamp with time zone not null,
-    end_date    timestamp with time zone not null,
-    created_at  timestamp with time zone not null
+    end_date    timestamp with time zone,
+    updated_at  timestamp with time zone not null default now()
 );
+
+create trigger set_timestamp
+    before update on companies
+    for each row
+execute function trigger_set_timestamp();
 
 comment on table companies is 'This table will contain all the companies I have been working in a defined time';
--- src/main/resources/db/migration/V003__create_company_positions_table.sql
+++ src/main/resources/db/migration/V003__create_company_positions_table.sql
@@ -9,12 +9,17 @@ create table company_positions
     description text             not null,
     current_job boolean                  not null,
     start_date  timestamp with time zone not null,
-    end_date    timestamp with time zone not null,
-    created_at  timestamp with time zone not null
+    end_date    timestamp with time zone,
+    updated_at  timestamp with time zone not null default now()
 );
 
 create index company_index
     on company_positions (company);
+
+create trigger set_timestamp
+    before update on company_positions
+    for each row
+execute function trigger_set_timestamp();
 
 
 comment on table company_positions is 'This table will contain all the positions inside a company in a defined time';
