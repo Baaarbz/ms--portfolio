6190ea65e640ccf2696baa9a80f52454e34141b5
barbz <eduardob907@gmail.com>
2023-11-04 00:12:06 +0100

    improvw validations

--- src/main/kotlin/com/barbzdev/portfolio/domain/job/Job.kt
+++ src/main/kotlin/com/barbzdev/portfolio/domain/job/Job.kt
@@ -2,7 +2,7 @@ package com.barbzdev.portfolio.domain.job
 
 import com.barbzdev.portfolio.domain.common.Month
 import com.barbzdev.portfolio.domain.common.Year
-import com.barbzdev.portfolio.domain.job.exception.InvalidDatesException
+import com.barbzdev.portfolio.domain.job.exception.InvalidJobDatesException
 import com.barbzdev.portfolio.domain.job.exception.InvalidJobDataException
 import java.net.URI
 import java.util.UUID
@@ -21,21 +21,21 @@ data class Job(
 
   init {
     if (joinEndDateAsNumber() != 0 && joinEndDateAsNumber() > joinStartDateAsNumber()) {
-      throw InvalidDatesException("job end date can not be after job start date")
+      throw InvalidJobDatesException("job end date can not be after job start date")
     }
     if (isCurrentCompany && joinEndDateAsNumber() > 0) {
-      throw InvalidDatesException("if is current job can not have end date")
+      throw InvalidJobDatesException("if is current job can not have end date")
     }
     if (!isCurrentCompany && joinEndDateAsNumber() == 0) {
-      throw InvalidDatesException("if is not current job must have defined an end date")
+      throw InvalidJobDatesException("if is not current job must have defined an end date")
     }
 
     jobData.positions.forEach { position ->
       if (position.joinEndDateAsNumber() > joinEndDateAsNumber()) {
-        throw InvalidDatesException("a position can not have and end date later than the job")
+        throw InvalidJobDatesException("a position can not have and end date later than the job")
       }
       if (joinStartDateAsNumber() > position.joinStartDateAsNumber()) {
-        throw InvalidDatesException("a position can not have and start earlier than the job")
+        throw InvalidJobDatesException("a position can not have and start earlier than the job")
       }
       if (!isCurrentCompany && position.isCurrentPosition) {
         throw InvalidJobDataException("if is not current company then any position can be current position")
