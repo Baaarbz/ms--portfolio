9995716745d0aedaa9354910d60ca994a9b104ff
barbz <eduardob907@gmail.com>
2023-11-06 22:32:54 +0100

    add OpenAPI configuration

---
+++ src/main/kotlin/com/barbzdev/portfolio/infrastructure/framework/configuration/OpenAPIConfiguration.kt
@@ -0,0 +1,32 @@
+package com.barbzdev.portfolio.infrastructure.framework.configuration
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class OpenAPIConfiguration {
+  @Bean
+  fun springShopOpenAPI(): OpenAPI {
+    return OpenAPI()
+      .info(
+        Info()
+          .title("Portfolio API")
+          .description("Rest API for my personal website")
+          .version("v0.0.1")
+          .license(
+            License()
+              .name("MIT")
+              .url("https://github.com/Baaarbz/ms--portfolio/blob/main/LICENSE")
+          )
+      )
+      .addServersItem(
+        Server()
+          .url("https://barbzdev.com/")
+      )
+  }
+}
